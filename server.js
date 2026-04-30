bde73eb363f47d9b8140e7cc0e272a2757aa90f0
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-29 23:10:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
